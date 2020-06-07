4a898ac331f769d07e132bd745ab393c88908063
Lenivene Bezerra <lenivene@msn.com>
2020-06-07 06:03:39 -0300

    add simple validation route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import multer from "multer";
+import { celebrate, Joi } from "celebrate";
 
 // Config
 import uploadConfig from "./config/upload";
@@ -12,7 +13,26 @@ const routes = Router();
 const upload = multer(uploadConfig);
 
 routes.get("/items", ItemsController.index);
-routes.post("/points", upload.single("image"), PointsController.create);
+routes.post(
+  "/points",
+  upload.single("image"),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      }),
+    },
+    { abortEarly: false }
+  ),
+  PointsController.create
+);
 routes.get("/points", PointsController.index);
 routes.get("/points/:id", PointsController.show);
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import cors from "cors";
 import path from "path";
+import { errors } from "celebrate";
 
 import routes from "./routes";
 
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(json());
 app.use(routes);
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+app.use(errors());
 
 app.listen(port, () => {
   console.log(`⚡️ Server listening on http://localhost:${port}`);
